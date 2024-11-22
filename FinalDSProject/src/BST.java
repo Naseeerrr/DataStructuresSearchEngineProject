41388bcd3aa83ff526e7c33409e730773e15e395
Naser :) <xnaser200@gmail.com>
2024-11-22 16:19:22 +0300

    bst node and bst

--- FinalDSProject/src/BST.java
+++ FinalDSProject/src/BST.java
@@ -1,18 +1,4 @@
-class BSTNode<T>{
-    public String key;  
-        public T data;  
-        public BSTNode<T> left,right;  
-      
-       
-        public BSTNode(String k,T val) {  
-            key=k;  
-            data=val;  
-            left=right=null;  
-        }  
-     
-     }  
-        
-     public class BST<T> {  
+public class BST<T> {  
          BSTNode<T> root, current;  
                
              
---
+++ FinalDSProject/src/BSTNode.java
@@ -0,0 +1,13 @@
+public class BSTNode<T>{
+    public String key;  
+        public T data;  
+        public BSTNode<T> left,right;  
+      
+       
+        public BSTNode(String k,T val) {  
+            key=k;  
+            data=val;  
+            left=right=null;  
+        }  
+     
+     }  
